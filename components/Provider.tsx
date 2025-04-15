97acd00bdce366376ba94ce7e3c8bb1ed342eb09
TommieTechie <49788015+TommieNg@users.noreply.github.com>
2025-04-14 23:50:41 -0400

    Add unstyled wallet login with Privy

---
+++ components/Provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId="cm9heti9z01ell70lye6yiwlj"
+      clientId="client-WY5iuNEjg97LH3Zhfa1eu3Leb4tkezN9a1xZ7GPHFXygV"
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: "light",
+          accentColor: "#676FFF",
+          logo: "https://your-logo-url",
+        },
+        // Create embedded wallets for users who don't have a wallet.
+        embeddedWallets: {
+          solana: { createOnLogin: "users-without-wallets" },
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
